8af6a2f1d8e9357f2e4475cb6a3dd4773a0795e9
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-26 20:08:38 +1100

    Commented out stun.xten.com as it is failing from travis

--- servers.js
+++ servers.js
@@ -15,10 +15,10 @@ exports.stun = [
   'stunserver.org',
   'stun.softjoys.com',
   'stun.voiparound.com',
-  'stun.voipbuster.com',
+  'stun.voipbuster.com'
   // 'stun.voipstunt.com',
   // 'stun.voxgratia.org',
-  'stun.xten.com'
+  // 'stun.xten.com'
 ];
 
 // TURN servers
