4345cf61a549c2153ee02be0abc0e823a55ad632
Benjamin Arthur Lupton <b@lupton.cc>
2014-01-29 13:45:50 -0500

    Added http://www.turnservers.com/

--- servers.js
+++ servers.js
@@ -12,6 +12,7 @@ exports.stun = [
   'stun.schlund.de',
   'stunserver.org',
   'stun.softjoys.com',
+  'stun.turnservers.com:3478',
   'stun.voiparound.com',
   'stun.voipbuster.com',
   'stun.voipstunt.com'
