0f7d76995644b2b3b2805c05c098906063d72c20
gaspard <gaspard@gmail.com>
2014-12-09 16:00:52 +0100

    readme: updating stun and turn files

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var normalice = require('normalice');
   ## Hey, don't use my STUN/TURN server!
 
   If for some reason your free STUN or TURN server ends up in the
-  [list](servers.js) of servers that is used in this module, you can feel
+  list of servers ([stun](stun.js) or [turn](turn.js)) that is used in this module, you can feel
   free to open an issue on this repository and those servers will be removed
   within 24 hours (or sooner).  This is the quickest and probably the most
   polite way to have something removed (and provides us some visibility
